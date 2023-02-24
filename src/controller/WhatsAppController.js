2c6c2b5119570628d4d0d951ebaf8094d2e095c4
joao.gimenes.clear <joao.gimenes.clear@gmail.com>
2023-02-24 12:28:33 -0300

    Feat: Events in the Attach Menu

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -72,6 +72,27 @@ class WhatsAppController{
         Element.prototype.hasClass = function(name){
            return this.classList.contains(name)
         }
+
+        HTMLFormElement.prototype.getForm = function () {
+
+            return new FormData(this);
+
+        }
+
+        HTMLFormElement.prototype.toJSON = function () {
+
+            let json = {};
+
+            this.getForm().forEach((value, key)=>{
+
+                json[key] = value;
+
+            });
+
+            return json;
+
+        }
+
     }
 
     initEvents(){
@@ -108,6 +129,149 @@ class WhatsAppController{
             this.el.panelAddContact.removeClass('open');
 
         })
+
+        this.el.photoContainerEditProfile.on('click', e=>{
+
+            this.el.inputProfilePhoto.click()
+
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e=>{
+
+            if (e.key === 'Enter'){
+                
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click()
+
+            }
+
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e=>{
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTML)
+
+        });
+
+        this.el.formPanelAddContact.on('submit', e=>{
+
+            e.preventDefault();
+
+            let formData = new FormData(this.el.formPanelAddContact);
+
+        });
+
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item =>{
+            
+            item.on('click', e=>{
+
+                this.el.home.hide();
+                this.el.main.css({
+                    display:'flex'
+                });
+
+            });
+
+        });
+
+        this.el.btnAttach.on('click', e=>{
+
+            e.stopPropagation();
+            this.el.menuAttach.addClass('open');
+            document.addEventListener('click', this.closeMenuAttach.bind(this))
+
+        })
+
+        this.el.btnAttachPhoto.on('click', e=>{
+            
+            this.el.inputPhoto.click();
+            
+        })
+        this.el.inputPhoto.on('change', e=>{
+
+            console.log(this.el.inputPhoto.files);
+
+            [...this.el.inputPhoto.files].forEach(file=>{
+
+                console.log(file);
+
+            })
+
+        });
+
+        this.el.btnAttachCamera.on('click', e=>{
+           
+            this.closeAllMainPanel();
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+                'height': 'calc(100% - 120px)'
+            });
+
+            this.el.btnClosePanelCamera.on('click', e=>{
+
+                this.closeAllMainPanel();
+                this.el.panelMessagesContainer.show();
+
+            });
+
+            this.el.btnTakePicture.on('click', e=>{
+
+                console.log('take picture');
+
+            })
+            
+        })
+
+        this.el.btnAttachDocument.on('click', e=>{
+
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+                'height': 'calc(100% - 120px)'
+            });
+
+        });
+
+        this.el.btnClosePanelDocumentPreview.on('click', e=>{
+
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+
+        })
+
+        this.el.btnSendDocument.on('click', e=>{
+
+            console.log('send document');
+
+        })
+
+        this.el.btnAttachContact.on('click', e=>{
+
+            this.el.modalContacts.show()
+            
+        });
+
+        this.el.btnCloseModalContacts.on('click', e=>{
+
+            this.el.modalContacts.hide();
+
+        })
+
+    }
+
+    closeAllMainPanel(){
+
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open')
+
+    }
+
+    closeMenuAttach(e){
+
+        document.removeEventListener('click', this.closeMenuAttach);
+        this.el.menuAttach.removeClass('open');
+        
 
     }
 
