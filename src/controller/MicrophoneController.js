e3198e9b7604ef366d0a7d9612d11c40e2ee1041
Joao Gimenes Clear <joao.gimenes.clear@gmail.com>
2023-03-06 10:06:09 -0300

    Feat: Recording and Sending Audio with attachment

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -64,24 +64,34 @@ export class MicrophoneController extends ClassEvent{
 
                 let filename = `rec${Date.now()}.webm`;
 
-                let file = new File([blob], filename, {
-                    type: this._mimeType,
-                    lastModified: Date.now()
-                });
-
-                console.log('file', file);
+                let audioContext = new AudioContext();
 
                 let reader = new FileReader();
 
                 reader.onload = e =>{
 
+                    audioContext.decodeAudioData(reader.result).then(decode=>{
+
+                        let file = new File([blob], filename, {
+                            type: this._mimeType,
+                            lastModified: Date.now()
+                        });
+
+                        this.trigger('recorded', (file, decode))
+
+                    })
+
+                   
+
                     let audio = new Audio(reader.result);
 
                     audio.play()
+                    
+                    reader.readAsDataURL(file);
 
-                }
+                   
+                }     
 
-                reader.readAsDataURL(file);
 
             });
 
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -801,6 +801,18 @@ export class WhatsAppController{
 
         this.el.btnFinishMicrophone.on('click',e=>{
 
+            this._microphoneController.on('recorded', (file, metadata)=>{
+
+                Message.sendAudio(
+                    this._contactActive.chatId,
+                    this.user.email,
+                    file,
+                    metadata,
+                    this._user.photo
+                )
+
+            })
+
             this._microphoneController.stopRecorder();
             this.closeRecordMicrophone();
 
