ba1ac2b4128c7cded99d2440f3186628a95ce41a
Joao Gimenes Clear <joao.gimenes.clear@gmail.com>
2023-03-07 08:46:57 -0300

    Feat: Added Desktop Alert notification with audio

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -15,13 +15,71 @@ export class WhatsAppController{
 
     constructor(){
 
+        this._active = true;
         console.log('WhatsAppController OK');
         this._firebase = new Firebase();
         this.initAuth();
         this.elementsPrototype();
         this.loadElements();
         this.initEvents();
+        this.checkNotifications();
         
+
+    }
+
+    checkNotifications(){
+
+        if (typeof Notification === 'function'){
+
+            if(Notification.permission !== 'granted'){
+
+                this.el.alertNotificationPermission.show();
+
+            } else {
+
+                this.el.alertNotificationPermission.hide()
+
+            } 
+
+            this.el.alertNotificationPermission.on('click', e=>{
+
+                Notification.requestPermission(permission=>{
+
+                    if (permission === 'granted'){
+
+                        this.el.alertNotificationPermission.hide();
+                        console.info('notificações permitidas');
+
+                    }
+
+                })
+
+            })
+
+        }
+
+    }
+
+    notification(data){
+
+        if (Notification.permission === 'granted' && !this._active){
+
+            let n = new Notification(this._contactActive.name, {
+                icon: this._contactActive.photo,
+                body: data.content
+            });
+
+            let sound = new Audio('./audio/alert.mp3');
+            sound.currentTime = 0;
+            sound.play();
+
+            setTimeout(()=>{
+
+                if (n) n.close();
+
+            }, 3000)
+
+        }
 
     }
 
@@ -199,6 +257,9 @@ export class WhatsAppController{
 
                 this.el.panelMessagesContainer.innerHTML = '';
 
+                this._messagesReceived = []
+
+
                 Message.getRef(this._contactActive.chatId).orderBy('timeStamp').onSnapshot(docs => {
 
                     let scrollTop = this.el.panelMessagesContainer.scrollTop;
@@ -206,6 +267,7 @@ export class WhatsAppController{
                     let scrollTopMax = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
 
                     let autoScroll = (scrollTop >= (scrollTopMax - 1));
+
 
                     docs.forEach(doc => {
 
@@ -217,6 +279,13 @@ export class WhatsAppController{
                         message.fromJSON(data);
 
                         let me = (data.from === this._user.email);
+
+                        if(!me && this._messagesReceived.filter(id =>{ return (id === data.id) }).length === 0) {
+
+                            this.notification(data);
+                            this._messagesReceived.push(data.id);
+
+                        }
 
                         let view = message.getViewElement(me);
 
@@ -390,6 +459,18 @@ export class WhatsAppController{
     }
 
     initEvents(){
+
+        window.addEventListener('focus', e=>{
+
+            this._active = true;
+
+        })
+
+        window.addEventListener('blur', e=>{
+
+            this._active = false;            
+
+        })
 
         this.el.inputSearchContacts.on('keyup', e=>{
 
